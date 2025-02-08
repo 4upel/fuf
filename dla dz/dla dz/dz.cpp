700781a29fe5f1cbf19680a8a299a42904f814c6
4upel <kirillnasik7@gmail.com>
2025-02-08 21:08:50 +0100

    новый проект для калькуляторинга

---
+++ dla dz/dla dz/dz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+
+int main() {
+
+
+
+
+
+
+
+}
--- proektik/proektik/chumachechi.cpp
+++ proektik/proektik/chumachechi.cpp
@@ -4,18 +4,32 @@ using namespace std ;
 
 int main() 
 {
-	string a;
+	setlocale(LC_ALL, "Rus");
 
-	setlocale(LC_ALL, "Rus");//забыл запятую
-
-	cout << "Hello world!" << endl;
-
-	cout << "Привет мир!" <<endl;// все остальное хорошо
-
-
-	cout << "Enter: ";
-	cin >> a;
-
-	cout << a << endl;
+	cout << "\tВ лесу родилась елочка!" << endl;
+	cout << "\tВ лесу она росла," << endl;
+	cout << "\tЗимой и летом стройная," << endl;
+	cout << "\tЗеленая была." << endl;
+	cout << endl;
+	cout << "\tВ лесу родилась елочка!" << endl;
+	cout << "\tВ лесу она росла," << endl;
+	cout << "\tЗимой и летом стройная," << endl;
+	cout << "\tЗеленая была." << endl;
+	cout << endl;
+	cout << "\tМетель ей пела песенку:" << endl;
+	cout << "\t«Спи, елочка, бай-бай!»" << endl;
+	cout << "\tМороз снежком укутывал:" << endl;
+	cout << "\t«Смотри, не замерзай!»" << endl;
+	cout << endl;
+	cout << "\tТрусишка зайка серенький" << endl;
+	cout << "\tПод елочкой скакал." << endl;
+	cout << "\tПорою волк, сердитый волк" << endl;
+	cout << "\tРысцою пробегал." << endl;
+	cout << endl;
+	cout << "\tЧу! Снег по лесу частому" << endl;
+	cout << "\tПод полозом скрипит," << endl;
+	cout << "\tЛошадка мохноногая" << endl;
+	cout << "\tТоропится, бежит." << endl;
+	cout << endl;
 	
 }
