fe9b3f51b89fb1f6202c00fc1ec385a611b3b6fc
4upel <kirillnasik7@gmail.com>
2025-02-09 06:15:16 +0100

    сделал калькулятор

--- dla dz/dla dz/dz.cpp
+++ dla dz/dla dz/dz.cpp
@@ -4,11 +4,36 @@ using namespace std;
 
 
 int main() {
+	setlocale(LC_ALL, "Rus");
+	double nomer1, nomer2, result;
+	char operacia;
+
+	cout << "Ведите число:\n";
+	cin >> nomer1;
+
+	cout << "Введите действие: +,-,*,/\n";
+	cin >> operacia;
+
+	cout << "Введите второе число:\n";
+	cin >> nomer2;
 	
+	if (operacia == '*') {
+		cout << nomer1 * nomer2;
 
+	}
 
+	if (operacia == '-') {
+		cout << nomer1 - nomer2;
 
+	}
+	
+	if (operacia == '/') {
+		cout << nomer1 / nomer2;
 
+	}
+	
+	if (operacia == '+') {
+		cout << nomer1 + nomer2;
 
-
+	}
 }
