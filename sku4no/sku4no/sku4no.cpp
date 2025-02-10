c2ce543fe894ee1550f257e2891cc9b27f3c0ec2
4upel <kirillnasik7@gmail.com>
2025-02-10 05:36:24 +0100

    скучно в 5 утра

---
+++ sku4no/sku4no/sku4no.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+using namespace std;
+
+short main() {
+	setlocale(LC_ALL, "Rus");
+	
+	
+	cout << "darov\n";
+	double a = 7.31;
+	cout << a << endl;
+	signed char b = '2';
+	cout << b << endl;
+	cout << ++a << endl;
+	
+	
+	
+	/*if (a != 7123.31)
+		cout << "neverno";
+	else
+	{
+		cout << "molodec";
+	}
+	*/
+}
