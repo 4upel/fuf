093193f39fc32602501bb42489ccec56d13ce3d6
4upel <kirillnasik7@gmail.com>
2025-02-12 03:21:27 +0100

    у меня все равно неправильно работал новый проект hw2 поэтому я сделал в старом

--- sku4no/hw2/hw2.cpp
+++ sku4no/hw2/hw2.cpp
@@ -3,20 +3,15 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Rus");
+	cout << "¬ведите число:";
+	
 
-	cout << "¬ведите число:\n";
-	int a;
-	cin >> a;
-	if (a > 0)
-	{
-		cout << "положительное";
-	}
-	if (a < 0)
-	{
-		cout << "отрицательное";
-	}
-	else
-	{
-		cout << "0";
-	}
+
+
+
+
+
+
+
+
 }
--- sku4no/sku4no/sku4no.cpp
+++ sku4no/sku4no/sku4no.cpp
@@ -3,24 +3,33 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	
+	float a, c;
+	char b;
+
+
 	cout << "Введите число:\n";
-	int a;
 	cin >> a;
-	if (a>0)
-	{
-		cout << "положительное";
-	}
-	if (a<0)
-	{
-		cout << "отрицательное";
-	}
-	else
-	{
-		cout << "0";
-	}
-	
-	
+	cout << "Введите операцию: (+, -, *, /,):\n";
+	cin >> b;
+	cout << "Введите второе число:\n";
+	cin >> c;
 
+	switch (b)
+	{
+	case'+':
+		cout << "Результат: " << a + c << endl;
+		break;
+	case'-':
+		cout << "Результат: " << a - c << endl;
+		break;
+	case'*':
+		cout << "Результат: " << a * c << endl;
+		break;
+	case'/':
+		cout << "Результат: " << a / c << endl;
+		break;
 
+	default:
+		cout << "Неверная операция";
+	}
 }
